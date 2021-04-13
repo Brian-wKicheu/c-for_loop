b66ae17f0108c8cef47f4329bc7c7d061ae21d94
Brian-wKicheu <78160253+Brian-wKicheu@users.noreply.github.com>
2021-04-13 16:54:52 +0300

    Update forLopNestMulitplication.cpp

--- forLopNestMulitplication.cpp
+++ forLopNestMulitplication.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-    for (int i = 1; i <=10; i++)
+    for (int i = 1; i <=10; i++)//outer for loop where each value multiplies all values in j
     {
-        for (int j = 1; j <=10; j++)
+        for (int j = 1; j <=10; j++)//inner loop where all values multiplies all values in i
         {
-           cout << i << " * "<<j<<" = "<<i * j<< endl;
+           cout << i << " * "<<j<<" = "<<i * j<< endl;//1*1=1, 1*2=2,,,1*10=10
         }
         cout << endl;
         
