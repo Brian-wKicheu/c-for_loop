d83fb200bdd0fc4d0a3898cdcbb8f77355ac6ba2
Brian-wKicheu <78160253+Brian-wKicheu@users.noreply.github.com>
2021-04-13 16:51:21 +0300

    Update factorialForLoop.cpp

--- factorialForLoop.cpp
+++ factorialForLoop.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
-
+//for loop is used if we know the exact value we are using
+//here we have declared number=6 then factorial initialised to 1
+//as factorial 1 is multiplied to i it loops then checks if i has decreased till it reaches 1
 int main(){
     int number = 6;
     int factorial =1;
